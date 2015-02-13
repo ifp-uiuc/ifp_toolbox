f7cc257694605cc15c756e3882c32fd9d40d68c5
Pooya Khorrami <pkhorrami4@gmail.com>
2015-02-13 00:02:56 -0600

    Minor modifications to FaceDetector class.

--- ifp_toolbox/faces/__init__.py
+++ ifp_toolbox/faces/__init__.py
@@ -2,20 +2,18 @@ import cv2
 import os
 
 class FaceDetector(object):
-
     def __init__(self, scale_factor=1.3, min_neighbors=5):
         module_path = os.path.dirname(__file__)
         classifier_path = os.path.join(module_path, 'haarcascade_frontalface_default.xml')
         self.detector = cv2.CascadeClassifier(classifier_path)      
-        print self.detector
         if self.detector.empty():
             raise Exception('Classifier xml file was not found.')
         self.scale_factor = scale_factor
         self.min_neighbors = min_neighbors
+        print self.detector
 
     def detect_faces(self, I):
         faces = self.detector.detectMultiScale(I, self.scale_factor, self.min_neighbors)
-        #print 'FACES: ', faces 
         return faces
 
     def crop_face_out(self, I, loc):
@@ -23,4 +21,4 @@ class FaceDetector(object):
         I_crop = I[y:y+h,x:x+h]
         return I_crop
 
-    
+ 
